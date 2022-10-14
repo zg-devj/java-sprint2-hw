ddde033904b7cc709a75275d6df24df82ad74bf4
Gleb Zakharov <webmaster-80@yandex.ru>
2022-10-14 09:06:58 +0300

    Добавлен вывод годового отчета в классе Main

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,48 @@
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Поехали!
+        MonthlyReport monthlyReport = new MonthlyReport();
+        YearlyReport yearlyReport = new YearlyReport();
+
         Scanner scanner = new Scanner(System.in);
         while (true) {
             printMenu();
             if (scanner.hasNextInt()) {
                 int command = scanner.nextInt();
-                if(command == 1) {
+                if (command == 1) {
+                    // Считать все месячные отчёты.
+                    monthlyReport.loadData();
+                } else if (command == 2) {
+                    // Считать годовой отчёт
+                    yearlyReport.loadData((short) 2021);
+                } else if (command == 3) {
 
-                } else if (command==2) {
+                } else if (command == 4) {
 
-                } else if (command==3) {
-
-                } else if (command==4) {
-
-                } else if (command==5) {
-
-                }
-                else {
+                } else if (command == 5) {
+                    // Вывести информацию о годовом отчёте
+                    if (yearlyReport.isDataLoaded()) {
+                        System.out.println("Годовой отчёт.");
+                        System.out.println("Год " + yearlyReport.getReportYear());
+                        System.out.println("Прибыль по каждому месяцу:");
+                        for (Map.Entry<Integer, Integer> profit : yearlyReport.getAllProfits().entrySet()) {
+                            System.out.println(
+                                    String.format("\t%s %d",
+                                            Service.getMonthNameByNumber(profit.getKey()),
+                                            profit.getValue()));
+                        }
+                        System.out.println(String.format(
+                                "Средний расход за все месяцы в году: %.2f",
+                                yearlyReport.getAverage(true)));
+                        System.out.println(String.format(
+                                "Средний доход за все месяцы в году: %.2f",
+                                yearlyReport.getAverage(false)));
+                    } else {
+                        System.out.println("Вы не считали годовой отчет!");
+                    }
+                } else {
                     System.out.println("Извините, такой команды пока нет");
                 }
             } else {
